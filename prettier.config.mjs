39003e0944823eabd0d10d42e426d3ce2a6bedbf
JoelTurtiainen <joel.turtiainen@gmail.com>
2025-08-22 10:04:02 +0300

    fix lint errors

---
+++ prettier.config.mjs
@@ -0,0 +1,20 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+
+const config = {
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  jsxSingleQuote: false,
+  bracketSameLine: false,
+  endOfLine: 'lf',
+}
+
+export default config
